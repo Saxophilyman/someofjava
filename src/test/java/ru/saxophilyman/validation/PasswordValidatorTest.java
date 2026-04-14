3e58ca4cec3afb9addc7ef0c99a0ba0645b81099
Saxophilyman <zd_@mail.ru>
2026-04-14 14:52:30 +0400

    Step-001

--- src/test/java/ru/saxophilyman/validation/PasswordValidatorTest.java
+++ src/test/java/ru/saxophilyman/validation/PasswordValidatorTest.java
@@ -1,4 +1,18 @@
 package ru.saxophilyman.validation;
 
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class PasswordValidatorTest {
+
+    @Test
+    void testErrorWhenPasswordIsnull() {
+        PasswordValidator validator = new PasswordValidator();
+
+        List<String> errors = validator.validate(null);
+        assertEquals(List.of("Password must not be null"), errors);
+    }
 }
