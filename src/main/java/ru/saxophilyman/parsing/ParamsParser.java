43a94f469a2ee66151952ddb248de3728c70ce2c
Saxophilyman <zd_@mail.ru>
2026-04-14 22:10:23 +0400

    Step-002 (test null)

--- src/main/java/ru/saxophilyman/parsing/ParamsParser.java
+++ src/main/java/ru/saxophilyman/parsing/ParamsParser.java
@@ -1,4 +1,12 @@
 package ru.saxophilyman.parsing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParamsParser {
+
+    Map<String, String> parse(String raw){
+        return new HashMap<String, String>();
+    }
+
 }
--- src/test/java/ru/saxophilyman/parsing/ParamsParserTest.java
+++ src/test/java/ru/saxophilyman/parsing/ParamsParserTest.java
@@ -1,4 +1,14 @@
 package ru.saxophilyman.parsing;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class ParamsParserTest {
+
+    @Test
+    void testErrorNull(){
+        ParamsParser parser = new ParamsParser();
+        assertThrows(IllegalArgumentException.class, () -> parser.parse(null));
+    }
 }
